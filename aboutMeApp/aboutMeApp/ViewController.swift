c828cc83f091c2893dc64985ecda2116f55464da
Kevin Tran <kevin2005tran@gmail.com>
2017-02-15 21:36:59 -0800

    first commit

---
+++ aboutMeApp/aboutMeApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  aboutMeApp
+//
+//  Created by Kevin Tran on 2/14/17.
+//  Copyright © 2017 Kevin Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+
+
