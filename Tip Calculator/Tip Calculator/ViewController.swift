e7b05b3168385f2977d872f98f65d195c54d8023
Kevin Tran <kevin2005tran@gmail.com>
2017-03-12 22:31:41 -0700

    created tip calculator app still need to customize the view controller finalize logic

--- Tip Calculator/Tip Calculator/ViewController.swift
+++ Tip Calculator/Tip Calculator/ViewController.swift
@@ -10,6 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var mealCost: Float = 0.0
+    var tipPercent: Float = 20.0
+    
+    func finalCost() -> Float {
+        return mealCost + tipPercent/100.0 * mealCost
+    }
+    
+    @IBOutlet weak var tipPercentLabel: UILabel!
+    @IBOutlet weak var finalCostLabel: UILabel!
+    @IBOutlet var costTextField: UITextField!
+    @IBAction func tipSliderChanged(sender: UISlider){
+        self.costTextField.text = "\(sender.value)"
+        self.tipPercentLabel.text = "\(self.tipPercent)"
+        self.finalCostLabel.text = "\(finalCost())"
+        
+    }
+    @IBAction func costTextFieldChanged(_ sender: UITextField) {
+        self.mealCost = Float(sender.text!)!
+        self.finalCostLabel.text = "\(finalCost())"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
