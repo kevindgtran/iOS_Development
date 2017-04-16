fa55e1c6291ceb8bfdb2c58c29b91a388dcaa22f
Kevin Tran <kevin2005tran@gmail.com>
2017-04-16 14:17:01 -0700

    adding dictionary algorithm practice lesson

---
+++ FUNDAMENTALS/XCODE_Examples/Firebase/Firebase_Udacity_wknd_chatapp/ios-nd-firebase-master/FriendlyChat/FriendlyChatSwift/Notes.swift
@@ -0,0 +1,16 @@
+//
+//  Notes.swift
+//  FriendlyChatSwift
+//
+//  Created by Kevin Tran on 4/1/17.
+//  Copyright © 2017 Google Inc. All rights reserved.
+//
+
+import Foundation
+
+//step 1. create firebase project, connect to xcode project (bundle id)
+//step 2. download googleService-Info.plist file, add file to Supporting Files folder so the app can communicate with the firebase project
+//3. open Podfile >> terminal >> open in xcode, install pods, this generates the xcode.workspace file - file that contains our project code and the pod file code
+//4. in order to communicate with firebase database make sure we have the necessary pods in our pod file 
+// 'Firebase/Database' & 'Firebase/Core' then $pod install if needed
+
