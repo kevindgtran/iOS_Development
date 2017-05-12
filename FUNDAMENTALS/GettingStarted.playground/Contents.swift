d8735400095a45ebacf0cc02f3a14721caaddcd0
Kevin Tran <kevin2005tran@gmail.com>
2017-05-11 20:55:10 -0700

    swift book fundamentals

---
+++ FUNDAMENTALS/GettingStarted.playground/Contents.swift
@@ -0,0 +1,68 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//var str = "Hello, playground"
+//
+
+//variables, constants
+//var newInstanceOfString = "Tran"
+//print(newInstanceOfString)
+//newInstanceOfString += " is the man!"
+//print(newInstanceOfString)
+//let numStopLights: Int = 4
+////numStopLights += 2
+
+//string interpolation
+////create town name
+//let townName: String = "KevinLand"
+////create town population
+//var townPopulation: Int = 10_500
+////create town description
+//var townDescription: String = "Welcome to \(townName) with a population of \(townPopulation) and \(numStopLights) stop lights"
+////create town unemployment rate, print updated desription
+//var townUnemployment: Double = 1.5
+//townDescription += ". The town has an unemployment rate of \(townUnemployment)."
+//print(townDescription)
+
+//Conditionals
+//var townPopulation: Int = 5_422
+//var message: String
+//var hasPostOffice: Bool = false
+//var hasDonutShop: Bool = false
+//
+//if townPopulation < 10_000 {
+//    message = "\(townPopulation) is a small town"
+//} else {
+//    message = "\(townPopulation) is a large town"
+//}
+//print(message)
+//
+//if !hasPostOffice {
+//    print("no post office, must go to next town for stamps")
+//} else {
+//    print("can buy stamps at the post office")
+//}
+//
+//if !hasDonutShop {
+//    print("no donut shop")
+//} else {
+//    print("donuts!")
+//}
+
+//Ternary 
+//var hasFood: Bool = true
+//hasFood ? print("there's food!"): print("no food")
+//
+////refactor above code to ternary
+//var townPopulation: Int = 5422
+//var message: String
+//
+//message = townPopulation < 10000 ? "\(townPopulation) is a small town": "\(townPopulation) is a large town"
+//print(message)
+
+
+
+
+
+
