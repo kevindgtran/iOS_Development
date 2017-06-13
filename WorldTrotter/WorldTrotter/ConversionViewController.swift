e44e41c5f66df5746a2f0ef87a401bd15d38aa75
Kevin Tran <kevin2005tran@gmail.com>
2017-06-12 20:09:25 -0700

    successfully added UITextFieldDelegate to round temperature decimal place to no greater than 1

--- WorldTrotter/WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -8,8 +8,17 @@
 
 import UIKit
 
-class ConversionViewController: UIViewController {
+class ConversionViewController: UIViewController, UITextFieldDelegate {
     
+    //create new instance of number formatter
+    var numberFormatter: NumberFormatter {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }
+
     //create an optional variable to store fahrenheit value of type Measurement<UnitTemperature>
     var fahrenheitValue: Measurement<UnitTemperature>? {
         //add property observer after property is changed
@@ -31,6 +40,14 @@ class ConversionViewController: UIViewController {
     @IBOutlet var celciusLabel: UILabel!
     @IBOutlet var textField: UITextField!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateCelsiusLabel()
+        
+    }
+    
+    
     @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
         if let text = textField.text, let value = Double(text) {
             fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
@@ -49,10 +66,27 @@ class ConversionViewController: UIViewController {
     func updateCelsiusLabel(){
         //check if celsius value has a value then update its label
         if let celsiusValue = celsiusValue {
-            celciusLabel.text = "\(celsiusValue.value)"
+            celciusLabel.text = numberFormatter.string(from: NSNumber(value: celsiusValue.value))
         } else {
             celciusLabel.text = "???"
         }
     }//end of updateCelsiusLabel function
+    
+    //implement the UITestFieldDelegate method
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        let existingTextHasDecimalSeperator = textField.text?.range(of: ".")
+        let replacementTextHasDecimalSeparator = string.range(of: ".")
+        
+        if existingTextHasDecimalSeperator != nil, replacementTextHasDecimalSeparator != nil {
+            return false
+        } else {
+            return true
+        }
+    }
+    
+    
+    
+    
     
 }//end of ConversionViewController
