83cde24292ca4c6dd5e717e5a88f2e31b1fb978a
Kevin Tran <kevin2005tran@gmail.com>
2017-06-13 22:54:26 -0700

    added UITabBarController and MKMap framework

---
+++ WorldTrotter/WorldTrotter/MapViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Kevin Tran on 6/13/17.
+//  Copyright © 2017 com.example. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        
+        print("MapViewController loaded its view")
+        
+    }//end of viewDidLoad
+    
+    
+    
+    
+}//end of MapViewController
