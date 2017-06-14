af5928764189acfaffef5c4d7e229642104eac27
Kevin Tran <kevin2005tran@gmail.com>
2017-06-14 11:10:01 -0700

    adding anchors and programmatic constraints

--- WorldTrotter/WorldTrotter/MapViewController.swift
+++ WorldTrotter/WorldTrotter/MapViewController.swift
@@ -18,9 +18,28 @@ class MapViewController: UIViewController {
 
     //override loadView() to create a new MKMapView - creating the view programmatically, set the MKMapView reference as the view
     override func loadView() {
+        
+        //new instance of MKMapView
         mapView = MKMapView()
 
+        //set view to instance of MKMapView
         view = mapView
+        
+        //new instance of UISegmentedControl
+        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+        
+        //UISegmentedControl background color, alpha
+        segmentedControl.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        
+        //UISegmentedControl default selection
+        segmentedControl.selectedSegmentIndex = 0
+        
+        //UISegmentedControl constraints to flase, so we can setup constaints manually
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        
+        //add UISegmentedControl to view
+        view.addSubview(segmentedControl)
+
     }
     
     
