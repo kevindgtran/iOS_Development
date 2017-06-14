9c76d75cf72ac93813581f6dde417586526fed26
Kevin Tran <kevin2005tran@gmail.com>
2017-06-14 10:39:05 -0700

    created MapKit View programmatically

--- WorldTrotter/WorldTrotter/MapViewController.swift
+++ WorldTrotter/WorldTrotter/MapViewController.swift
@@ -7,13 +7,27 @@
 //
 
 import UIKit
+import MapKit //import the MapKit library programmatically
 
 class MapViewController: UIViewController {
     
+    //create a reference for MKMapView
+    //MARK: - Properties
+    var mapView: MKMapView!
+    
+
+    //override loadView() to create a new MKMapView - creating the view programmatically, set the MKMapView reference as the view
+    override func loadView() {
+        mapView = MKMapView()
+
+        view = mapView
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
-        
         print("MapViewController loaded its view")
-        
     }//end of viewDidLoad
     
     
