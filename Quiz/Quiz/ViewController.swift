65862b81ac65185274dfb6dd31759a43d905f9ac
Kevin Tran <kevin2005tran@gmail.com>
2017-06-19 16:29:49 -0700

    debugging tutorial

--- Quiz/Quiz/ViewController.swift
+++ Quiz/Quiz/ViewController.swift
@@ -54,20 +54,17 @@ class ViewController: UIViewController {
         self.nextQuestionLabelCenterXConstraint.constant = 0
         self.currentQuestionLabelCenterXConstraint.constant += screenWidth
         
-        UIView.animate(withDuration: 0.5,
-                       delay: 0,
-                       options: [.curveLinear],
-                       animations: {
-                        self.currentQuestionLabel.alpha = 0
-                        self.nextQuestionLabel.alpha = 1
-                        
-                        self.view.layoutIfNeeded()
+        UIView.animate(withDuration: 0.5, delay: 0, options: [.curveLinear], animations: {
+            self.currentQuestionLabel.alpha = 0
+            self.nextQuestionLabel.alpha = 1
+
+            self.view.layoutIfNeeded()
         },
-                       completion: { _ in
-                        swap(&self.currentQuestionLabel, &self.nextQuestionLabel)
-                        swap(&self.currentQuestionLabelCenterXConstraint, &self.nextQuestionLabelCenterXConstraint)
-                        
-                        self.updateOffScreenLabel()
+            completion: { _ in
+            swap(&self.currentQuestionLabel, &self.nextQuestionLabel)
+            swap(&self.currentQuestionLabelCenterXConstraint, &self.nextQuestionLabelCenterXConstraint)
+            
+            self.updateOffScreenLabel()
         })
     }
     
