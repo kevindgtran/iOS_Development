259af7cfac7d742d6f9c192061c141514ce51511
Kevin Tran <kevin2005tran@gmail.com>
2017-08-10 12:23:32 -0700

    practice with Obj-C

---
+++ Obj-c/AGoodStart/AGoodStart/main.c
@@ -0,0 +1,21 @@
+//
+//  main.c
+//  AGoodStart
+//
+//  Created by Kevin Tran on 8/7/17.
+//  Copyright © 2017 com.example. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{ //beginning of main function
+    //print the beginning of the novel
+    printf("It was the best of times.\n");
+    printf("It was the worse of times. \n");
+    /* Is that acutally any good?
+     Maybe it needs a rewrite. */
+    
+    return 0;
+    
+} //end of main function
