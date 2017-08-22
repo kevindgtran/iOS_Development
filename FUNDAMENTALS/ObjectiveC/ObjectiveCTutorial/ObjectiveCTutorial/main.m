69bfd70f77a006758089d1ba3d73990e770f8048
Kevin Tran <kevin2005tran@gmail.com>
2017-08-22 13:32:02 -0700

    objective c chapter 1

---
+++ FUNDAMENTALS/ObjectiveC/ObjectiveCTutorial/ObjectiveCTutorial/main.m
@@ -0,0 +1,35 @@
+//
+//  main.m
+//  ObjectiveCTutorial
+//
+//  Created by Kevin Tran on 8/21/17.
+//  Copyright © 2017 com.example. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {//function called on app launch
+    @autoreleasepool {//ObjC ARC - clears unused memory
+        
+        //print statement
+        //NSLog(@"Hello World");
+        
+        //create an empty string pointer, nil == nonexistant object in memory
+        //NSString *nothing = nil;
+        //NSLog(@"nothing is located at %p", nothing);
+        
+        //create a pointer to a string in memory
+        //NSString *quote = @"dogs have masters while cats have staff.";
+        //log the quotes length using the NSString length method
+        //NSLog(@"The quote length is: %d", (int)[quote length]);
+        
+        //log the character at index 5
+        //NSString *newQuote = @"dogs rule and cats drool";
+        //log the 5th indece with the correct string formatter (%_)
+        //NSLog(@"The newQuotes character at index 5 is: %c", (char)[newQuote characterAtIndex:5]);
+        
+        
+        
+    }
+    return 0;
+}
