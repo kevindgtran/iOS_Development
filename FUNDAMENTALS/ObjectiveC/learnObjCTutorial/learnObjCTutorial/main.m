604829d51d2e7c51f78b6410983c3d9f57808b50
Kevin Tran <kevin2005tran@gmail.com>
2017-08-24 14:29:55 -0700

    more obj-c practice

---
+++ FUNDAMENTALS/ObjectiveC/learnObjCTutorial/learnObjCTutorial/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  learnObjCTutorial
+//
+//  Created by Kevin Tran on 8/24/17.
+//  Copyright © 2017 com.example. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
