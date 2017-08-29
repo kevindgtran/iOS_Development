004f8f347bd51e48f1d77a0d2a27f9f785adad3f
Kevin Tran <kevin2005tran@gmail.com>
2017-08-28 20:28:42 -0700

    objectiveC tutorial 2

---
+++ FUNDAMENTALS/ObjectiveC/ProgrammingInObjC/prog1/prog1/main.m
@@ -0,0 +1,23 @@
+//
+//  main.m
+//  prog1
+//
+//  Created by Kevin Tran on 8/28/17.
+//  Copyright © 2017 com.example. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Programming is fun!");
+        NSLog(@"And programming in Objective-C is even more fun!");
+        NSLog(@"Testing...\n..1\n...2\n....3");
+        
+        int sum;
+        sum = 25 + 50;
+        NSLog(@"25 plus 50 is %i", sum);
+    }
+    return 0;
+}
