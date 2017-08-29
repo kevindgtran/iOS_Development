993219693ed3d7a0eff0117b3c2673b8a45afb5b
Kevin Tran <kevin2005tran@gmail.com>
2017-08-29 14:29:42 -0700

    objc tutorial 2

--- FUNDAMENTALS/ObjectiveC/ProgrammingInObjC/prog1/prog1/main.m
+++ FUNDAMENTALS/ObjectiveC/ProgrammingInObjC/prog1/prog1/main.m
@@ -10,14 +10,51 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Programming is fun!");
-        NSLog(@"And programming in Objective-C is even more fun!");
-        NSLog(@"Testing...\n..1\n...2\n....3");
+ 
+//        NSLog(@"Programming is fun!");
+//        NSLog(@"Programming in Objective-C is even more fun!");
         
-        int sum;
-        sum = 25 + 50;
-        NSLog(@"25 plus 50 is %i", sum);
+//        NSLog(@"Testing...\n.1\n..2\n...3");
+        
+//        int sum;
+//        
+//        sum = 50 + 25;
+//        NSLog(@"50 plus 25 is %i", sum);
+        
+//        int value1, value2, totalValues;
+//        
+//        value1 = 20;
+//        value2 = 30;
+//        totalValues = value2 + value1;
+//        
+//        NSLog(@"%i plus %i is %i", value1, value2, totalValues);
+        
+//        NSLog(@"In Objective-C, lowercase letters are significant.\nmain is where program execution begins.\nOpen and closed braces enclose program statements in a routine.\nAll program statements must be terminated by a semicolon.");
+        
+//        int i;
+//        i = 1;
+//        
+//        NSLog(@"Testing...\n...%i\n....%i\n.....%i", i, i + 1, i + 2);
+        
+//        int sum;
+//        sum = 87 - 15;
+//        NSLog(@"87 subtract 15 is equal to %i", sum);
+        
+//        int sum;
+//        /* COMPUTE RESULT */
+//        sum = 25 + 37 - 19;
+//        
+//        //DISPLAY RESULT
+//        NSLog(@"The answer is %i", sum);
+        
+//        int answer, result;
+//        
+//        answer = 100;
+//        result = answer - 10;
+//        
+//        NSLog(@"The result is %i", result + 5);
+        
+        
     }
     return 0;
 }
