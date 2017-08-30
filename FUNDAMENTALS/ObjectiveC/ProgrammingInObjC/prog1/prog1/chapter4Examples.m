6c50b072c11f4d42ea4b733770d12e8a29f31db2
Kevin Tran <kevin2005tran@gmail.com>
2017-08-30 16:24:04 -0700

    data types

---
+++ FUNDAMENTALS/ObjectiveC/ProgrammingInObjC/prog1/prog1/chapter4Examples.m
@@ -0,0 +1,27 @@
+//
+//  chapter4Examples.m
+//  prog1
+//
+//  Created by Kevin Tran on 8/30/17.
+//  Copyright © 2017 com.example. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, char * argv[])
+{
+    @autoreleasepool {
+        //creating variables of different types
+        int integerVar = 10;
+        float decimalVar = 3.15;
+        double doubleVar = 1.000000003;
+        char charVar = 'e';
+        
+        NSLog(@"Newly created variables are %i, %f, %f, %c", integerVar, decimalVar, doubleVar, charVar);
+        
+        
+        
+        
+    }
+    return 0;
+}
---
+++ FUNDAMENTALS/ObjectiveC/ProgrammingInObjC/prog1/prog1/exercise2.m
@@ -0,0 +1,72 @@
+////
+////  Exercise2.m
+////  prog1
+////
+////  Created by Kevin Tran on 8/30/17.
+////  Copyright © 2017 com.example. All rights reserved.
+////
+//
+//#import <Foundation/Foundation.h>
+//
+////interface section
+//@interface XYPoint : NSObject
+//
+//-(int) xValue;
+//-(int) yValue;
+//-(void) setXValue;
+//-(void) setYValue;
+//-(void) print;
+//
+//@end
+//
+////implementation section
+//@implementation XYPoint
+//{
+//    int xValue;
+//    int yValue;
+//}
+//
+//-(void) setXValue: (int) x
+//{
+//    xValue = x;
+//}
+//
+//-(void) setYValue: (int) y
+//{
+//    yValue = y;
+//}
+//
+//-(int) xValue
+//{
+//    return xValue;
+//}
+//
+//-(int) yValue
+//{
+//    return yValue;
+//}
+//
+//-(void) print
+//{
+//    NSLog(@"Coordinates: (%i, %i)", xValue, yValue);
+//}
+//
+//@end
+//
+////program section
+//int main (int argc, char * argv[])
+//{
+//    @autoreleasepool {
+//        
+//        //create class instance
+//        XYPoint *MyCurrentPoint = [[XYPoint alloc] init];
+//        
+//        [MyCurrentPoint setYValue: 4];
+//        [MyCurrentPoint setXValue: 5];
+//        
+//        [MyCurrentPoint print];
+//        
+//    }
+//    
+//    return 0;
+//}
