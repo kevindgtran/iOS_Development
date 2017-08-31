353fe7f6a4c6864a8d841677150e5dd3a96ed297
Kevin Tran <kevin2005tran@gmail.com>
2017-08-31 16:58:56 -0700

    creating more classes

--- FUNDAMENTALS/ObjectiveC/ProgrammingInObjC/prog1/prog1/chapter4Examples.m
+++ FUNDAMENTALS/ObjectiveC/ProgrammingInObjC/prog1/prog1/chapter4Examples.m
@@ -6,11 +6,11 @@
 //  Copyright © 2017 com.example. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
-
-int main (int argc, char * argv[])
-{
-    @autoreleasepool {
+//#import <Foundation/Foundation.h>
+//
+//int main (int argc, char * argv[])
+//{
+//    @autoreleasepool {
 //        //creating variables of different types
 //        int integerVar = 10;
 //        float decimalVar = 3.15;
@@ -61,34 +61,57 @@ int main (int argc, char * argv[])
 //        sum = eight % seven;
 //        NSLog(@"%i %% %i = %i", eight, seven, sum);
         
-        //examples of type casting
-        int num1 = 1;
-        int num2 = 2;
-        float num3 = 3.5;
-        float num4 = 4.7;
-        int intSum;
-        float floatSum;
+//        //examples of type casting
+//        int num1 = 1;
+//        int num2 = 2;
+//        float num3 = 3.5;
+//        float num4 = 4.7;
+//        int intSum;
+//        float floatSum;
+//        
+//        //convert values to int sum
+//        intSum = (int) num3 + (int) num4 + (int) 3.78;
+//        NSLog(@"%f + %f + 3 = %i", num3, num4, intSum);
+//        
+//        //convert values to float sum
+//        floatSum = (float) num1 + (float) num2;
+//        NSLog(@"%i + %i = %f", num1, num2, floatSum);
+//        
+//        //create variables, update their values with the assignment operators, test on console
+//        int number1 = 3;
+//        int number2 = 7;
+//        
+//        number1 += 9;
+//        number2 += 17;
+//        
+//        NSLog(@"%i + %i = %i", number1, number2, number1 + number2);
         
-        //convert values to int sum
-        intSum = (int) num3 + (int) num4 + (int) 3.78;
-        NSLog(@"%f + %f + 3 = %i", num3, num4, intSum);
+        //create a program that converts 27F to degrees Celcius, using the formula C = (F - 32)/ 1.8
         
-        //convert values to float sum
-        floatSum = (float) num1 + (float) num2;
-        NSLog(@"%i + %i = %f", num1, num2, floatSum);
+//        int degreesF = 27;
+//        int degreesC = (degreesF - 32) / 1.8;
+//        
+//        NSLog(@"%i degrees Fahrenheit is %i degrees Celcius", degreesF, degreesC);
         
-        //create variables, update their values with the assignment operators, test on console
-        int number1 = 3;
-        int number2 = 7;
+//        char c, d;
+//        
+//        c = 'd';
+//        d = c;
+//        
+//        NSLog(@"d = %c", d);
         
-        number1 += 9;
-        number2 += 17;
+//        //write a program to evaluate this polynomial, 3x^3 - 5x^2 + 6, x = 2.55
+//        float x = 2.55;
+//        float result = (3 * (x * x * x)) - (5 * (x * x)) + 6;
+//        
+//        NSLog(@"f(%f) = 3x^3 - 5x^2 + 6 equals: %f", x, result);
+  
+
+//        long double result = (3.31 * 0.00000001 + 2.01 * 0.0000001) / (7.16 * 0.000001 + 2.01 * 0.00000001);
+//        
+//        NSLog(@"The very small result is: %Lf", result);
         
-        NSLog(@"%i + %i = %i", number1, number2, number1 + number2);
         
-        
-        
-        
-    }
-    return 0;
-}
+//    }
+//    return 0;
+//}
---
+++ FUNDAMENTALS/ObjectiveC/ProgrammingInObjC/prog1/prog1/chapter4Exercises.m
@@ -0,0 +1,71 @@
+////
+////  chapter4Exercises.m
+////  prog1
+////
+////  Created by Kevin Tran on 8/31/17.
+////  Copyright © 2017 com.example. All rights reserved.
+////
+//
+//#import <Foundation/Foundation.h>
+//
+////declare a class
+//@interface Complex : NSObject
+//
+//-(void) setReal: (double) a;
+//-(void) setImaginary: (double) b;
+//-(void) print; //display as a + bi
+//-(double) real;
+//-(double) imaginary;
+//
+//@end
+//
+////define its methods
+//@implementation Complex
+//{
+//    double real;
+//    double imaginary;
+//}
+//-(double) real
+//{
+//    return real;
+//}
+//
+//-(double) imaginary
+//{
+//    return imaginary;
+//}
+//
+//-(void) setReal: (double) a
+//{
+//    real = a;
+//}
+//
+//-(void) setImaginary:(double) b
+//{
+//    imaginary = b;
+//}
+//
+//-(void) print
+//{
+//    NSLog(@"%f + %e", real, imaginary);
+//}
+//
+//@end
+//
+////test it in a program
+//int main (int argc, char * argv[])
+//{
+//    @autoreleasepool {
+//        Complex *myComplexNo;
+//        myComplexNo = [Complex alloc];
+//        myComplexNo = [myComplexNo init];
+//        
+//        [myComplexNo setReal: 3];
+//        [myComplexNo setImaginary: 15];
+//        
+//        [myComplexNo print];
+//     
+//    }
+//    return 0;
+//}
+//
