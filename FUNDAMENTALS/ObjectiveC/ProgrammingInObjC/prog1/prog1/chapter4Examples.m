e9066129dd888a5934e6d22a0c9d7d0ff88d5e60
Kevin Tran <kevin2005tran@gmail.com>
2017-08-31 14:23:00 -0700

    data type arithmatic practice

--- FUNDAMENTALS/ObjectiveC/ProgrammingInObjC/prog1/prog1/chapter4Examples.m
+++ FUNDAMENTALS/ObjectiveC/ProgrammingInObjC/prog1/prog1/chapter4Examples.m
@@ -11,35 +11,55 @@
 int main (int argc, char * argv[])
 {
     @autoreleasepool {
-        //creating variables of different types
-        int integerVar = 10;
-        float decimalVar = 3.15;
-        double doubleVar = 1.000000003;
-        char charVar = 'e';
+//        //creating variables of different types
+//        int integerVar = 10;
+//        float decimalVar = 3.15;
+//        double doubleVar = 1.000000003;
+//        char charVar = 'e';
+//        
+//        NSLog(@"Newly created variables are %i, %f, %f, %c", integerVar, decimalVar, doubleVar, charVar);
         
-        NSLog(@"Newly created variables are %i, %f, %f, %c", integerVar, decimalVar, doubleVar, charVar);
+//        //declare a new variable for each of the different Obj-C tyes and use them in an NSLog sentence
+//        
+//        int number;
+//        number = 12;
+//        
+//        float smallDec;
+//        smallDec = 3.1415;
+//        
+//        double bigDec;
+//        bigDec = 25.123456;
+//        
+//        char oneLetter;
+//        oneLetter = 'k';
+//        
+//        NSLog(@"number is: %i", number);
+//        NSLog(@"smallDec is: %f", smallDec);
+//        NSLog(@"bigDec is: %f", bigDec);
+//        NSLog(@"oneLetter is: %c", oneLetter);
         
-        //declare a new variable for each of the different Obj-C tyes and use them in an NSLog sentence
+        //create different values and do some arithmatic
+        int four = 4;
+        int three = 3;
+        int five = 5;
+        int seven = 7;
+        int eight = 8;
+        int sum;
         
-        int number;
-        number = 12;
+        sum = four + three;
+        NSLog(@"%i + %i = %i", four, three, sum);
         
-        float smallDec;
-        smallDec = 3.1415;
+        sum = seven - three;
+        NSLog(@"%i - %i = %i", seven, three, sum);
         
-        double bigDec;
-        bigDec = 25.123456;
+        sum = four * five;
+        NSLog(@"%i * %i = %i", four, five, sum);
         
-        char oneLetter;
-        oneLetter = 'k';
+        sum = eight / four;
+        NSLog(@"%i / %i = %i", eight, four, sum);
         
-        NSLog(@"number is: %i", number);
-        NSLog(@"smallDec is: %f", smallDec);
-        NSLog(@"bigDec is: %f", bigDec);
-        NSLog(@"oneLetter is: %c", oneLetter);
-        
-        
-        
+        sum = eight % seven;
+        NSLog(@"%i %% %i = %i", eight, seven, sum);
         
         
         
