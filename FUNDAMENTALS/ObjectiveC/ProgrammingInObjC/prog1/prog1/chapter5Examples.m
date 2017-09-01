41f8473ad5ffe5611331c0ccf65a1f27409d0448
Kevin Tran <kevin2005tran@gmail.com>
2017-08-31 17:55:24 -0700

    iterating with for loops scanf routine

---
+++ FUNDAMENTALS/ObjectiveC/ProgrammingInObjC/prog1/prog1/chapter5Examples.m
@@ -0,0 +1,60 @@
+//
+//  chapter5Examples.m
+//  prog1
+//
+//  Created by Kevin Tran on 8/31/17.
+//  Copyright © 2017 com.example. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, char * argv[])
+{
+    @autoreleasepool {
+        
+//        //create program that is the sum of all values up to 200, using a for loop
+//        int n;
+//        int result = 0;
+//        
+//        for (n = 1; n <= 200; n += 1) {
+//            result += n;
+//        }
+//        
+//        NSLog(@"The sum of all the values up to 200 is: %i", result);
+
+//        //Build a program that creates a table showing the sum of the first 10 numbers
+//        NSLog(@"TABLE OF TRIANGULAR NUMBERS");
+//        NSLog(@" n    Sum from 1 to n");
+//        NSLog(@"---  ------------------");
+//        
+//        int n;
+//        int total = 0;
+//        
+//        for (n = 1; n <= 10; ++n){
+//            total += n;
+//            NSLog(@"%2i        %i", n, total);
+//        }
+        
+//        //Build a program that asks the user to enter a numeric value then display the sum of all the numbers up to that value
+//        int n;
+//        int userNumber;
+//        int total = 0;
+//        
+//        
+//        NSLog(@"enter a number");
+//        scanf("%i", &userNumber);
+//        
+//        for(n = 1; n <= userNumber; ++n){
+//            total += n;
+//        }
+//        
+//        NSLog(@"You chose %i, all the numbers added up to %i equals: %i", userNumber, userNumber, total);
+        
+        
+        
+        
+        
+    }
+    
+    return 0;
+}
