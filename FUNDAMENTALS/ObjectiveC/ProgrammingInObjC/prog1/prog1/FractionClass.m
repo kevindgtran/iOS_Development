1224b388e5214ce2df46ef7b0f2a42a215cdfd84
Kevin Tran <kevin2005tran@gmail.com>
2017-09-06 13:03:24 -0700

    updating class

---
+++ FUNDAMENTALS/ObjectiveC/ProgrammingInObjC/prog1/prog1/FractionClass.m
@@ -0,0 +1,77 @@
+//
+//  FractionClass.m
+//  prog1
+//
+//  Created by Kevin Tran on 9/6/17.
+//  Copyright © 2017 com.example. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Fraction : NSObject
+
+-(void) print;
+-(void) setNumerator: (int) n;
+-(void) setDenominator: (int) d;
+-(int) numerator;
+-(int) denominator;
+-(double) convertToNum;
+
+@end
+
+@implementation Fraction
+{
+    int numerator;
+    int denominator;
+    double convertToNum;
+}
+
+-(void) print
+{
+    NSLog(@"%i/%i", numerator, denominator);
+}
+
+-(void) setNumerator:(int) n
+{
+    numerator = n;
+}
+
+-(void) setDenominator:(int) d
+{
+    denominator = d;
+}
+
+-(int) numerator
+{
+    return numerator;
+}
+
+-(int) denominator
+{
+    return denominator;
+}
+
+-(double) convertToNum
+{
+    return (double) numerator/denominator;
+}
+
+@end
+
+
+int main (int argc, char * argv[])
+{
+    @autoreleasepool {
+    
+        Fraction *MyFraction = [[Fraction alloc] init];
+        
+        [MyFraction setNumerator: 1];
+        [MyFraction setDenominator:2];
+        [MyFraction print];
+        NSLog(@"%f", [MyFraction convertToNum]);
+     
+    }
+    return 0;
+}
+
+
---
+++ FUNDAMENTALS/ObjectiveC/ProgrammingInObjC/prog1/prog1/chapter6Examples.m
@@ -0,0 +1,38 @@
+////
+////  chapter6Examples.m
+////  prog1
+////
+////  Created by Kevin Tran on 9/6/17.
+////  Copyright © 2017 com.example. All rights reserved.
+////
+//
+//#import <Foundation/Foundation.h>
+//
+////create a program that asks the user for a number, the returns its absolute value
+////ask the user for a number, store number in userResponse variable
+////check if number is a negative, if true then convert to positive and return
+//
+//int main (int argc, char * argv[])
+//{
+//    @autoreleasepool {
+//      
+////        int userResponse;
+////        
+////        NSLog(@"Enter a number value");
+////        scanf("%i", &userResponse);
+////        
+////        if (userResponse < 0) {
+////            userResponse = -(userResponse);
+////        }
+////        
+////        NSLog(@"The absolute value is %i", userResponse);
+//        
+//        
+//        
+//        
+//        
+//        
+//        
+//    }// end of autoreleasepool
+//    return 0;
+//}
