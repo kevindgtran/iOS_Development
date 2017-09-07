d2a65f6dc68d2639401e682dbc25d2a307013089
Kevin Tran <kevin2005tran@gmail.com>
2017-09-07 15:51:31 -0700

    objc class inheritance tutorial

--- FUNDAMENTALS/ObjectiveC/ProgrammingInObjC/prog1/prog1/chapter6Exercises.m
+++ FUNDAMENTALS/ObjectiveC/ProgrammingInObjC/prog1/prog1/chapter6Exercises.m
@@ -1,39 +1,31 @@
+////
+////  chapter6Exercises.m
+////  prog1
+////
+////  Created by Kevin Tran on 9/6/17.
+////  Copyright © 2017 com.example. All rights reserved.
+////
 //
-//  chapter6Exercises.m
-//  prog1
+//#import <Foundation/Foundation.h>
 //
-//  Created by Kevin Tran on 9/6/17.
-//  Copyright © 2017 com.example. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
-
-int main (int argc, char * argv[])
-{
-    @autoreleasepool {
-        
-//        //build an application that asks the user for 2 numbers, see if the first number is evenly divisible by the second number and display the appropriate message
-//        int firstNum;
-//        int secondNum;
+//int main (int argc, char * argv[])
+//{
+//    @autoreleasepool {
 //        
-//        NSLog(@"please enter 2 numbers");
-//        scanf("%i %i", &firstNum, &secondNum);
+////        //build an application that asks the user for 2 numbers, see if the first number is evenly divisible by the second number and display the appropriate message
+////        int firstNum;
+////        int secondNum;
+////        
+////        NSLog(@"please enter 2 numbers");
+////        scanf("%i %i", &firstNum, &secondNum);
+////        
+////        if (firstNum % secondNum == 0) {
+////            NSLog(@"%i is evenly divisible by %i", firstNum, secondNum);
+////        } else {
+////            NSLog(@"%i is not evenly divisible by %i", firstNum, secondNum);
+////        }
+//
 //        
-//        if (firstNum % secondNum == 0) {
-//            NSLog(@"%i is evenly divisible by %i", firstNum, secondNum);
-//        } else {
-//            NSLog(@"%i is not evenly divisible by %i", firstNum, secondNum);
-//        }
-
-      
-        
-        
-        
-        
-        
-        
-        
-        
-    }//end of autoreleasepool
-    return 0;
-}
+//    }//end of autoreleasepool
+//    return 0;
+//}
