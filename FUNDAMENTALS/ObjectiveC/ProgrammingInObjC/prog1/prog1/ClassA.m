6bcbd9ca274af58b64e5a8a64502f6c944fdf52f
Kevin Tran <kevin2005tran@gmail.com>
2017-09-08 13:47:58 -0700

    prime algorithm in Objective C

--- FUNDAMENTALS/ObjectiveC/ProgrammingInObjC/prog1/prog1/ClassA.m
+++ FUNDAMENTALS/ObjectiveC/ProgrammingInObjC/prog1/prog1/ClassA.m
@@ -1,57 +1,57 @@
+////
+////  ClassA.m
+////  prog1
+////
+////  Created by Kevin Tran on 9/7/17.
+////  Copyright © 2017 com.example. All rights reserved.
+////
 //
-//  ClassA.m
-//  prog1
+//#import <Foundation/Foundation.h>
 //
-//  Created by Kevin Tran on 9/7/17.
-//  Copyright © 2017 com.example. All rights reserved.
+////ClassA declaration and definition
+//@interface ClassA : NSObject
+//{
+//    int x; //public variable declaration
+//}
+//-(void) initVar;
 //
-
-#import <Foundation/Foundation.h>
-
-//ClassA declaration and definition
-@interface ClassA : NSObject
-{
-    int x; //public variable declaration
-}
--(void) initVar;
-
-@end
-
-@implementation ClassA
--(void) initVar
-{
-    x = 100;
-}
-@end
-
-//ClassB declaration and definition
-@interface ClassB : ClassA
--(void) printVar;
-
-@end
-
-@implementation ClassB
--(void) printVar
-{
-    NSLog(@"x = %i", x);
-}
-
-@end
-
-int main (int argc, char * argv[])
-{
-    @autoreleasepool {
-        
-        ClassB *myClass = [[ClassB alloc] init];
-        
-        [myClass initVar];
-        [myClass printVar];
-        
-    }
-    return 0;
-}
-
-
-
-
-
+//@end
+//
+//@implementation ClassA
+//-(void) initVar
+//{
+//    x = 100;
+//}
+//@end
+//
+////ClassB declaration and definition
+//@interface ClassB : ClassA
+//-(void) printVar;
+//
+//@end
+//
+//@implementation ClassB
+//-(void) printVar
+//{
+//    NSLog(@"x = %i", x);
+//}
+//
+//@end
+//
+//int main (int argc, char * argv[])
+//{
+//    @autoreleasepool {
+//        
+//        ClassB *myClass = [[ClassB alloc] init];
+//        
+//        [myClass initVar];
+//        [myClass printVar];
+//        
+//    }
+//    return 0;
+//}
+//
+//
+//
+//
+//
--- FUNDAMENTALS/ObjectiveC/ProgrammingInObjC/prog1/prog1/chapter6Exercises.m
+++ FUNDAMENTALS/ObjectiveC/ProgrammingInObjC/prog1/prog1/chapter6Exercises.m
@@ -1,31 +1,47 @@
-////
-////  chapter6Exercises.m
-////  prog1
-////
-////  Created by Kevin Tran on 9/6/17.
-////  Copyright © 2017 com.example. All rights reserved.
-////
 //
-//#import <Foundation/Foundation.h>
+//  chapter6Exercises.m
+//  prog1
 //
-//int main (int argc, char * argv[])
-//{
-//    @autoreleasepool {
+//  Created by Kevin Tran on 9/6/17.
+//  Copyright © 2017 com.example. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, char * argv[])
+{
+    @autoreleasepool {
+        
+//        //build an application that asks the user for 2 numbers, see if the first number is evenly divisible by the second number and display the appropriate message
+//        int firstNum;
+//        int secondNum;
 //        
-////        //build an application that asks the user for 2 numbers, see if the first number is evenly divisible by the second number and display the appropriate message
-////        int firstNum;
-////        int secondNum;
-////        
-////        NSLog(@"please enter 2 numbers");
-////        scanf("%i %i", &firstNum, &secondNum);
-////        
-////        if (firstNum % secondNum == 0) {
-////            NSLog(@"%i is evenly divisible by %i", firstNum, secondNum);
-////        } else {
-////            NSLog(@"%i is not evenly divisible by %i", firstNum, secondNum);
-////        }
-//
+//        NSLog(@"please enter 2 numbers");
+//        scanf("%i %i", &firstNum, &secondNum);
 //        
-//    }//end of autoreleasepool
-//    return 0;
-//}
+//        if (firstNum % secondNum == 0) {
+//            NSLog(@"%i is evenly divisible by %i", firstNum, secondNum);
+//        } else {
+//            NSLog(@"%i is not evenly divisible by %i", firstNum, secondNum);
+//        }
+        
+        //build a program that displays a table of all the prime numbers up to 50
+        //loop through 2-50
+        //check each if prime - if any number below it is evenly divisible, if so not prime, display only prime numbers
+        
+        int p, d, isPrime;
+        for (p = 2; p <= 50; ++p) {
+            isPrime = 1;
+            
+            for (d = 2; d < p; ++d)
+                if (p % d == 0)
+                    isPrime = 0;
+            
+                if (isPrime != 0)
+                    NSLog(@"%i", p);
+           
+        }
+       
+    }//end of autoreleasepool
+    return 0;
+}
