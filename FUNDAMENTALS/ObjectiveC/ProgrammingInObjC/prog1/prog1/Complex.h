63287e0d02800bfd1ab97cf204794f1ca7028fb6
Kevin Tran <kevin2005tran@gmail.com>
2017-09-18 11:05:34 -0700

    creating custom initializer methods

--- FUNDAMENTALS/ObjectiveC/ProgrammingInObjC/prog1/prog1/Complex.h
+++ FUNDAMENTALS/ObjectiveC/ProgrammingInObjC/prog1/prog1/Complex.h
@@ -1,46 +1,46 @@
+////
+////  Complex.h
+////  prog1
+////
+////  Created by Kevin Tran on 9/13/17.
+////  Copyright © 2017 com.example. All rights reserved.
+////
 //
-//  Complex.h
-//  prog1
+//@interface Complex : NSObject
 //
-//  Created by Kevin Tran on 9/13/17.
-//  Copyright © 2017 com.example. All rights reserved.
+//@property double real, imaginary;
 //
-
-@interface Complex : NSObject
-
-@property double real, imaginary;
-
--(void) print;
--(void) setReal: (double) r setImaginary: (double) i;
--(Complex *) add: (Complex *) f;
-
-@end
-
-
-@implementation Complex
-
-@synthesize real, imaginary;
-
--(void) print
-{
-    NSLog(@"%f, %f", real, imaginary);
-}
-
--(void) setReal:(double)r setImaginary:(double)i
-{
-    real = r;
-    imaginary = i;
-}
-
--(Complex *) add:(Complex *) f
-{
-    Complex *result = [[Complex alloc] init];
-    
-    result.real = real + f.real;
-    result.imaginary = imaginary + f.imaginary;
-    
-    return result;
-    
-}
-
-@end
+//-(void) print;
+//-(void) setReal: (double) r setImaginary: (double) i;
+//-(Complex *) add: (Complex *) f;
+//
+//@end
+//
+//
+//@implementation Complex
+//
+//@synthesize real, imaginary;
+//
+//-(void) print
+//{
+//    NSLog(@"%f, %f", real, imaginary);
+//}
+//
+//-(void) setReal:(double)r setImaginary:(double)i
+//{
+//    real = r;
+//    imaginary = i;
+//}
+//
+//-(Complex *) add:(Complex *) f
+//{
+//    Complex *result = [[Complex alloc] init];
+//    
+//    result.real = real + f.real;
+//    result.imaginary = imaginary + f.imaginary;
+//    
+//    return result;
+//    
+//}
+//
+//@end
--- FUNDAMENTALS/ObjectiveC/ProgrammingInObjC/prog1/prog1/Complex.m
+++ FUNDAMENTALS/ObjectiveC/ProgrammingInObjC/prog1/prog1/Complex.m
@@ -1,41 +1,41 @@
+////
+////  Complex.m
+////  prog1
+////
+////  Created by Kevin Tran on 9/13/17.
+////  Copyright © 2017 com.example. All rights reserved.
+////
 //
-//  Complex.m
-//  prog1
+//#import <Foundation/Foundation.h>
+//#import "Fraction.h"
+//#import "Complex.h"
 //
-//  Created by Kevin Tran on 9/13/17.
-//  Copyright © 2017 com.example. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
-#import "Fraction.h"
-#import "Complex.h"
-
-int main (int argc, char * argv[])
-{
-    @autoreleasepool {
-        
-        //Example of polymorphism
-        Fraction *f1 = [[Fraction alloc] init];
-        Fraction *f2 = [[Fraction alloc] init];
-        
-        NSLog(@"The f2 is of type: %@", [f2 class]);
-        
-        Fraction *fractionResult;
-        
-        Complex *c1 = [[Complex alloc] init];
-        Complex *c2 = [[Complex alloc] init];
-        
-        Complex *compResult;
-        
-        //Example of Dynamic Binding and the id Type
-//        id dataValue;
+//int main (int argc, char * argv[])
+//{
+//    @autoreleasepool {
+//        
+//        //Example of polymorphism
 //        Fraction *f1 = [[Fraction alloc] init];
+//        Fraction *f2 = [[Fraction alloc] init];
+//        
+//        NSLog(@"The f2 is of type: %@", [f2 class]);
+//        
+//        Fraction *fractionResult;
+//        
 //        Complex *c1 = [[Complex alloc] init];
-        //first dataValue gets set to a Fraction value
-        //then dataValue gets set to a Complex value
-        
-        
-        
-    }
-    return 0;
-}
+//        Complex *c2 = [[Complex alloc] init];
+//        
+//        Complex *compResult;
+//        
+//        //Example of Dynamic Binding and the id Type
+////        id dataValue;
+////        Fraction *f1 = [[Fraction alloc] init];
+////        Complex *c1 = [[Complex alloc] init];
+//        //first dataValue gets set to a Fraction value
+//        //then dataValue gets set to a Complex value
+//        
+//        
+//        
+//    }
+//    return 0;
+//}
