a87637ac3ede7a8ddd8dea14afec62bd20e090e8
Kevin Tran <kevin2005tran@gmail.com>
2017-09-19 14:32:40 -0700

    creating categories in ObjC

--- FUNDAMENTALS/ObjectiveC/ProgrammingInObjC/prog1/prog1/Fraction.h
+++ FUNDAMENTALS/ObjectiveC/ProgrammingInObjC/prog1/prog1/Fraction.h
@@ -1,88 +1,67 @@
-////
-////  Fraction.h
-////  prog1
-////
-////  Created by Kevin Tran on 9/13/17.
-////  Copyright © 2017 com.example. All rights reserved.
-////
 //
-//@interface Fraction : NSObject
+//  Fraction.h
+//  prog1
 //
-//-(void) print;
-//-(void) setNumerator: (int) n;
-//-(void) setDenominator: (int) d;
-//-(void) setTo: (int) n over: (int) d;
-//-(int) numerator;
-//-(int) denominator;
-//-(double) convertToNum;
-//-(instancetype) initWith: (int) n over: (int) d;
+//  Created by Kevin Tran on 9/13/17.
+//  Copyright © 2017 com.example. All rights reserved.
 //
-//@end
-//
-//@implementation Fraction
-//{
-//    int numerator;
-//    int denominator;
-//    double convertToNum;
-//}
-//
-//-(void) setTo:(int)n over:(int)d
-//{
-//    numerator = n;
-//    denominator = d;
-//}
-//
-////initialization method
-//-(instancetype) initWith: (int) n over: (int) d
-//{
-//    self = [super init];
-//    
-//    if (self) {
-//        [self setTo: n over: d];
-//    }
-//    
-//    return self;
-//}
-//
-//
-//-(void) print
-//{
-//    if (denominator == 1) {
-//        NSLog(@"%i", numerator);
-//    } else if (numerator == 0) {
-//        NSLog(@"zero");
-//    } else {
-//        NSLog(@"%i/%i", numerator, denominator);
-//    }
-//}
-//
-//-(void) setNumerator:(int) n
-//{
-//    numerator = n;
-//}
-//
-//-(void) setDenominator:(int) d
-//{
-//    denominator = d;
-//}
-//
-//-(int) numerator
-//{
-//    return numerator;
-//}
-//
-//-(int) denominator
-//{
-//    return denominator;
-//}
-//
-//-(double) convertToNum
-//{
-//    if (denominator != 0){
-//        return (double) numerator/denominator;
-//    } else {
-//        return NAN;
-//    }
-//}
-//
-//@end
+
+@interface Fraction : NSObject
+
+@property int numerator, denominator;
+
+-(void) print;
+-(void) setTo: (int) n over: (int) d;
+-(double) convertToNum;
+-(instancetype) initWith: (int) n over: (int) d;
+
+@end
+
+@implementation Fraction
+{
+    int numerator;
+    int denominator;
+    double convertToNum;
+}
+
+@synthesize numerator, denominator;
+
+-(void) setTo:(int)n over:(int)d
+{
+    numerator = n;
+    denominator = d;
+}
+
+//initialization method
+-(instancetype) initWith: (int) n over: (int) d
+{
+    self = [super init];
+    
+    if (self) {
+        [self setTo: n over: d];
+    }
+    
+    return self;
+}
+
+-(void) print
+{
+    if (denominator == 1) {
+        NSLog(@"%i", numerator);
+    } else if (numerator == 0) {
+        NSLog(@"zero");
+    } else {
+        NSLog(@"%i/%i", numerator, denominator);
+    }
+}
+
+-(double) convertToNum
+{
+    if (denominator != 0){
+        return (double) numerator/denominator;
+    } else {
+        return NAN;
+    }
+}
+
+@end
