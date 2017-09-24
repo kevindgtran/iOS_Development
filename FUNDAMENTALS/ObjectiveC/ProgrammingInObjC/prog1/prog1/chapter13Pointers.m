d3cd23e3dfb3f3f6973a1c159033ef97cd1d0131
Kevin Tran <kevin2005tran@gmail.com>
2017-09-23 18:34:15 -0700

    ObjectiveC Pointers

---
+++ FUNDAMENTALS/ObjectiveC/ProgrammingInObjC/prog1/prog1/chapter13Pointers.m
@@ -0,0 +1,69 @@
+//
+//  chapter13Pointers.m
+//  prog1
+//
+//  Created by Kevin Tran on 9/23/17.
+//  Copyright © 2017 com.example. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, char * argv[])
+{
+    @autoreleasepool {
+        
+        //declare 3 variables assigning each the reference pointer of the previous and print the values
+        
+        int count = 10, x;
+        int *intPnt;         //intPnt declared to be of type "pointer to int"
+        intPnt = &count;     //intPnt variable set to the address operator of count, which is 10. Address operator points to that variables value
+        x = *intPnt;         //x variable set to the indirect operator, which tells the system intPnt is a pointer to another data item
+        NSLog(@"The value of x = %i, count = %i", x, count);
+        
+        //declare 2 variables, assign a character to one, then assign the 2nd to the address operator of the first. Print both variables, Change the first variable then print both, change the first variable again and print both out
+        char c = 'k';                           //the character variable c is defined and initialized to 'k'
+        char *cPtr = &c;                        //the cPtr variable is defined to be of type "pointer to char" - meaning whatever value is stored in cPtr should be an                                 indirect reference / pointer to char, which is assigned the address operator / pointer to the variable c
+        NSLog(@"c = %c, cPtr = %c", c, *cPtr);  //c is printing its value, while *cPtr is printing the reference / pointer
+        c = 'B';
+        NSLog(@"c = %c, cPtr = %c", c, *cPtr);
+        c = '$';
+        NSLog(@"c = %c, cPtr = %c", c, *cPtr);
+        
+        //Now change value that cPtr points to...
+        *cPtr = 'G';
+        NSLog(@"c = %c, cPtr = %c", c, *cPtr);   //this changes the value that cPtr is referencing / pointing to, henceforth changes the value of c
+        
+        //create a struct then a variable that references that struct
+        struct Date
+        {
+            int month, day, year;
+        };
+        struct Date today;
+        struct Date *tomorrow;
+        
+        tomorrow = &today;
+        (*tomorrow).day = 24;    //the pointer is in parenthesis becuase the dot operation has higher precedence
+                                 //becuase pointers to structs are so common, there's the "structure pointer" operator '->'
+        
+        //Create a struct named CalendarDate with 3 properties: day, month, year. Setup a pointer to the struct, set its values using the structure pointer operator and print the date to the console.
+        struct CalendarDate
+        {
+            int day, month, year;
+        };
+        struct CalendarDate holiday;
+        struct CalendarDate *thanksGiving;
+        
+        thanksGiving = &holiday;
+        thanksGiving->day = 23;
+        thanksGiving->month = 11;
+        thanksGiving->year = 2017;
+        NSLog(@"This year Thanksgiving will on %i/%i/%i", holiday.month, (*thanksGiving).day, thanksGiving->year % 100);
+        
+        
+        
+        
+        
+        
+    }
+    return 0;
+}
--- FUNDAMENTALS/ObjectiveC/ProgrammingInObjC/prog1/prog1/chapter13Structures.m
+++ FUNDAMENTALS/ObjectiveC/ProgrammingInObjC/prog1/prog1/chapter13Structures.m
@@ -1,52 +1,66 @@
+////
+////  chapter13Structures.m
+////  prog1
+////
+////  Created by Kevin Tran on 9/22/17.
+////  Copyright © 2017 com.example. All rights reserved.
+////
 //
-//  chapter13Structures.m
-//  prog1
+//#import <Foundation/Foundation.h>
 //
-//  Created by Kevin Tran on 9/22/17.
-//  Copyright © 2017 com.example. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
-
-int main (int argc, char * argv[])
-{
-    @autoreleasepool {
-        
-        //Declare a new struct type named Date with 3 variables of integer type
-        struct Date
-        {
-            int month;
-            int day, year;
-        };
-        
-        //Declare new variables of our Date type
-        struct Date myBirthday;
-        struct Date today;
-    
-        //Set the dates for the 2 new variables
-        myBirthday.day = 2;
-        myBirthday.month = 10;
-        myBirthday.year = 1987;
-        NSLog(@"My Birthday is on %i/%i/%i", myBirthday.month, myBirthday.day, myBirthday.year);
-        
-        today.day = 22;
-        today.month = 9;
-        today.year = 2017;
-        NSLog(@"Today's date is %i/%i/%i", today.month, today.day, today.year);
-        
-        //declare a struct type named Humidity with 4 properties: Current, Low, High and currentTemp, set values and print the humidity readings to the console
-        struct Humidity
-        {
-            int currentHumidity, low, high, currentTemp;
-        };
-        
-        struct Humidity aptHumidity;
-        aptHumidity.currentHumidity = 43;
-        aptHumidity.low = 40;
-        aptHumidity.high = 55;
-        aptHumidity.currentTemp = 70;
-        
-        NSLog(@"The humidity readings are current humidity: %i%%, low: %i%%, high: %i%%, current temperature: %i%%", aptHumidity.currentHumidity, aptHumidity.low, aptHumidity.high, aptHumidity.currentTemp);
-    }
-    return 0;
-}
+//int main (int argc, char * argv[])
+//{
+//    @autoreleasepool {
+//        
+//        //Declare a new struct type named Date with 3 variables of integer type
+//        struct Date
+//        {
+//            int month;
+//            int day, year;
+//        };
+//        
+//        //Declare new variables of our Date type
+//        struct Date myBirthday;
+//        struct Date today;
+//    
+//        //Set the dates for the 2 new variables
+//        myBirthday.day = 2;
+//        myBirthday.month = 10;
+//        myBirthday.year = 1987;
+//        NSLog(@"My Birthday is on %i/%i/%i", myBirthday.month, myBirthday.day, myBirthday.year);
+//        
+//        today.day = 22;
+//        today.month = 9;
+//        today.year = 2017;
+//        NSLog(@"Today's date is %i/%i/%i", today.month, today.day, today.year);
+//        
+//        //declare a struct type named Humidity with 4 properties: Current, Low, High and currentTemp, set values and print the humidity readings to the console
+//        struct Humidity
+//        {
+//            int currentHumidity, low, high, currentTemp;
+//        };
+//        
+//        struct Humidity aptHumidity;
+//        aptHumidity.currentHumidity = 43;
+//        aptHumidity.low = 40;
+//        aptHumidity.high = 55;
+//        aptHumidity.currentTemp = 70;
+//        
+//        NSLog(@"The humidity readings are current humidity: %i%%, low: %i%%, high: %i%%, current temperature: %i%%", aptHumidity.currentHumidity, aptHumidity.low, aptHumidity.high, aptHumidity.currentTemp);
+//        
+//        //Initializing all the properties of a struct is similar to initializing an array by setting its values in curly brackets in the order of the variables
+//        struct Date tomorrow = { 9, 24, 2017 };
+//        NSLog(@"Tomorrows date is %i/%i/%i", tomorrow.month, tomorrow.day, tomorrow.year);
+//        
+//        //Declare a new struct named BBQTemps with the cooking temps for meats, then set their values using the dot notation method
+//        struct BBQTemps
+//        {
+//            int poultry, beef, pork;
+//        };
+//        
+//        struct BBQTemps bbqTemperatureGuide = { .poultry = 165, .beef = 145, .pork = 145 };
+//        NSLog(@"According to USDA's minimum internal cooking temperatures for meat are: \nPoultry = %i°\nBeef = %i°\nPork = %i°", bbqTemperatureGuide.poultry, bbqTemperatureGuide.beef, bbqTemperatureGuide.pork );
+//        
+//    }
+//    return 0;
+//}
