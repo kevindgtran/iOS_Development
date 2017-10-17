0dc77601c1b86da28cfb58686e8601b392b73d46
Kevin Tran <kevin2005tran@gmail.com>
2017-10-16 19:25:49 -0700

    delegates and protocols practice

--- Fundamentals/iOS_Programming_Basics/Delegates-Protocols/Delegates-Protocols/Delegates-Protocols/SelectionViewController.swift
+++ Fundamentals/iOS_Programming_Basics/Delegates-Protocols/Delegates-Protocols/Delegates-Protocols/SelectionViewController.swift
@@ -8,8 +8,15 @@
 
 import UIKit
 
-class SelectionViewController: UIViewController {
+//1.
+protocol listOfOrders {
+    func makeTheseChanges(_ image: UIImage, _ backgroundColor: UIColor, _ name: String)
+}
 
+class SelectionViewController: UIViewController {
+    //2.
+    var ordersDelegate: listOfOrders!
+    
     //MARK: - Properties
     @IBOutlet weak var imperialBtn: UIButton!
     @IBOutlet weak var rebelBtn: UIButton!
@@ -21,12 +28,24 @@ class SelectionViewController: UIViewController {
     
     //MARK: - Actions
     @IBAction func imperialBtnPress(_ sender: UIButton) {
+        //3.
+        ordersDelegate.makeTheseChanges(UIImage(named: "Darth Vader")!, UIColor.red, "Darth Vador")
         dismiss(animated: true, completion: nil)
     }
     
     @IBAction func RebelBtnPress(_ sender: UIButton) {
+        ordersDelegate.makeTheseChanges(UIImage(named: "Luke Skywalker")!, UIColor.cyan, "Luke Skywalker")
         dismiss(animated: true, completion: nil)
     }
-
-    
 }
+
+
+//Delegates and Protocols setup
+//Delegator (MGR)
+//1. In the Delegator / "Manager" / decision maker VC, create our protocol (list of orders - functions)
+//2. Assign the Protocol (list of orders) to a variable (will be given to an intern soon)
+//3. On the "Manager's actions", tell the delegatee (intern) to perform the protocol actions with these specifics
+
+//Delegatee(Intern)
+//4. In the Delegatee VC, get the ordersDelegate variable and set to self. This says "Hey Mgr! I will be your delegatee and follow your orders!"
+//5. Delegates have to conform to the protocol == "Have to be an employee of the MGR to complete the commands (good to put into extension). State the function to complete and pass in the "order specifics" from the boss
--- Fundamentals/iOS_Programming_Basics/Delegates-Protocols/Delegates-Protocols/Delegates-Protocols/ViewController.swift
+++ Fundamentals/iOS_Programming_Basics/Delegates-Protocols/Delegates-Protocols/Delegates-Protocols/ViewController.swift
@@ -25,7 +25,27 @@ class ViewController: UIViewController {
         //segue to view with ID "SelectionVC"
         let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
         let selectionViewController = storyBoard.instantiateViewController(withIdentifier: "SelectionVC") as! SelectionViewController
+        //4.
+        selectionViewController.ordersDelegate = self
         self.present(selectionViewController, animated: true, completion: nil)
     }
 }
 
+//5.
+extension ViewController: listOfOrders {
+    func makeTheseChanges(_ image: UIImage, _ backgroundColor: UIColor, _ name: String) {
+        img.image = image
+        label.text = name
+        view.backgroundColor = backgroundColor
+    }
+}
+
+//Delegates and Protocols setup
+//Delegator (MGR)
+//1. In the Delegator / "Manager" / decision maker VC, create our protocol (list of orders - functions)
+//2. Assign the Protocol (list of orders) to a variable (will be given to an intern soon)
+//3. On the "Manager actions", tell the delegatee (intern) to perform the protocol actions with these specifics
+
+//Delegatee(Intern)
+//4. In the Delegatee VC, get the ordersDelegate variable and set to self. This says "Hey Mgr! I will be your delegatee and follow your orders!"
+//5. Delegates have to conform to the protocol == "Have to be an employee of the MGR to complete the commands (good to put into extension). State the function to complete and pass in the "order specifics" from the boss
